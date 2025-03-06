dfe0d08b1ecdf079ac7f7e8b2220e5fef8bd7789
Nico <ntorchaire@weareloom.fr>
2025-03-06 13:14:29 +0100

    males

--- Enclos.java
+++ Enclos.java
@@ -1,18 +1,14 @@
 class Enclos {
     List<Pingouins> pingouins = new ArrayList();
-
     Enclos(List<Pingouins> pingouins) {
         this.pingouins.addAll(pingouins);
     }
-
     void addPingouin(Pingouin pingouin) {
         pingouins.add(pingouin);
     }
-
     int countPingouins() {
         return pingouins.size();
     }
-
     List<Pingouins> getMale() {
         List<Pingouins> males = new ArrayList();
         for (Pingouin pingouin : pingouins) {
@@ -23,4 +19,3 @@ class Enclos {
         return males;
     }
 }
-
