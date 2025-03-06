e7cdcf6a328a3ff516452457183120f918c13bcd
Nico <ntorchaire@weareloom.fr>
2025-03-06 13:21:31 +0100

    indent

--- Pingouin.java
+++ Pingouin.java
@@ -2,14 +2,21 @@ class Pingouin {
 
 	String nom;
 	Sexe sexe;
+<<<<<<< HEAD
 	Couleur couleur;
 
 	Pingouin(String nom, Sexe sexe, Couleur couleur) {
+=======
+	String couleur;
+
+	Pingouin(String nom, Sexe sexe, String couleur) {
+>>>>>>> 2900dca (indent)
 		this.nom = nom;
 		this.sexe = sexe;
 		this.couleur = couleur;
 	}
 
+<<<<<<< HEAD
 	Couleur getCouleur() {
 		return couleur;
 	}
@@ -18,8 +25,17 @@ class Pingouin {
 		return sexe;
 	}
 }
+=======
+	String getCouleur() {
+	return couleur;
+	}
 
-boolean isMale() {
-return sexe == Sexe.MALE
-}
+	String getSexe() {
+	return sexe;
+	}
+>>>>>>> 2900dca (indent)
+
+	boolean isMale() {
+		return sexe == Sexe.MALE
+	}
 }
