3cddaab75fe98da6d0b31759ced43a0f3ccee67e
Nico <ntorchaire@weareloom.fr>
2025-03-06 13:14:46 +0100

    oups

--- Enclos.java
+++ Enclos.java
@@ -18,7 +18,7 @@ class Enclos {
 	List<Pingouins> getMale() {
 		List<Pingouins> males = new ArrayList();
 		for(Pingouin pingouin : pingouins) {
-			if (pingouin.getSexe == Sexe.FEMELLE) {
+			if (pingouin.getSexe == Sexe.MALE) {
 				males.add(pingouin);
 			}
 		}
