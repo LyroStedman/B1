74bedc657f5f134f374a6fa1cddbc5ef50c8765a
Nico <ntorchaire@weareloom.fr>
2025-03-06 12:50:10 +0100

    Fonctions retournants le sexe et la couleur

--- Pingouin.java
+++ Pingouin.java
@@ -1,11 +1,20 @@
 class Pingouin {
 
-String nom;
-Sexe sexe;
-String couleur;
+	String nom;
+	Sexe sexe;
+	Couleur couleur;
 
-Pingouin(String nom, Sexe sexe, String couleur) {
-	this.nom = nom;
-	this.sexe = sexe;
-	this.couleur = couleur;
+	Pingouin(String nom, Sexe sexe, Couleur couleur) {
+		this.nom = nom;
+		this.sexe = sexe;
+		this.couleur = couleur;
+	}
+
+	Couleur getCouleur() {
+		return couleur;
+	}
+
+	Sexe getSexe() {
+		return sexe;
+	}
 }
