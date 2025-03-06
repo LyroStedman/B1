eb3ed55b79af2794c321d48ca5ca523b965e43d6
Nico <ntorchaire@weareloom.fr>
2025-03-06 13:23:39 +0100

    Ajouté methode pour compter les pengouins dans Enclos.java

--- Enclos.java
+++ Enclos.java
@@ -4,4 +4,14 @@ class Enclos {
 	Enclos(List<Pingouins> pingouins) {
 		this.pingouins.addAll(pingouins);
 	}
+
+
+	void addPingouin(Pingouin pingouin) {
+		pingouins.add(pingouin);
+	}
+
+	int countPingouins() {
+		return pingouins.size();
+	}
+
 }
