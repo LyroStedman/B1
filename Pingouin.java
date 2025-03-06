b565569c17c6efd1069ce90b032b4b3e23c30eeb
Nico <ntorchaire@weareloom.fr>
2025-03-06 12:50:10 +0100

    Connaitre le sexe d'un pingouin

--- Pingouin.java
+++ Pingouin.java
@@ -9,3 +9,11 @@ Pingouin(String nom, Sexe sexe, String couleur) {
 	this.sexe = sexe;
 	this.couleur = couleur;
 }
+
+String getCouleur() {
+return couleur;
+}
+
+String getSexe() {
+return sexe;
+}
