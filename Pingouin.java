b877f64686767a3d6c140d549a194820e919c990
hectoreltractor <testtesttest@gmail.com>
2025-11-18 06:09:49 -0800

    Class pingouin toute seule

--- Pingouin.java
+++ Pingouin.java
@@ -1,11 +1,9 @@
 class Pingouin {
 
 String nom;
-Sexe sexe;
 String couleur;
 
-Pingouin(String nom, Sexe sexe, String couleur) {
+Pingouin(String nom, String couleur) {
 	this.nom = nom;
-	this.sexe = sexe;
 	this.couleur = couleur;
 }
