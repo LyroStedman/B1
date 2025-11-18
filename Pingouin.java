97cfbe625f3b3905df89ab2ec37ef2eded5283b1
Valen <vlnmp88@gmail.com>
2025-11-18 14:53:09 +0100

    On ajoute la methode sexe a Pingouin.java

--- Pingouin.java
+++ Pingouin.java
@@ -4,8 +4,23 @@ String nom;
 Sexe sexe;
 String couleur;
 
+
 Pingouin(String nom, Sexe sexe, String couleur) {
 	this.nom = nom;
 	this.sexe = sexe;
 	this.couleur = couleur;
+
+	Pingouin(String nom, Sexe sexe, Couleur couleur) {
+		this.nom = nom;
+		this.sexe = sexe;
+		this.couleur = couleur;
+	}
+
+	Couleur getCouleur() {
+		return couleur;
+	}
+
+	Sexe getSexe() {
+		return sexe;
+	}
 }
